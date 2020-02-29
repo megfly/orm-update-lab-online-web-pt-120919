2e572490ef655ccc0fd132b8ca904a575b3ee2dd
Meghan Flynn <mflynn333@gmail.com>
2020-02-29 00:19:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,12 +50,11 @@ class Student
     student
   end
   
-  def self.new_from_db([])
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
   
   def self.find_by_name(name)
